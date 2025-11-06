d01b60296d2d3fc1ce39599628b7466431ae7774
mariescheffmann <mariemadsen26@yahoo.dk>
2025-11-06 11:27:17 +0100

    Test setup for opcua client and mockup opcua server

--- Edge/main.py
+++ Edge/main.py
@@ -1,6 +1,33 @@
-def main():
-    print("Hello World :))")
+from opcua import Client
+from dotenv import load_dotenv
+import os
+import time
 
 
-if __name__ == "__main__":
-    main()
+load_dotenv()
+url = os.getenv("OPC_UA_URL")
+if not url:
+    raise ValueError("OPC_UA_URL is not set in .env")
+
+client = Client(url)
+
+variables = {
+    "ServerStatus": "ns=2;i=2",
+    "CurrentTime": "ns=2;i=3",
+}
+
+try:
+    client.connect()
+    print("Connected to server")
+
+    while True:
+        for name, node_id in variables.items():
+            node = client.get_node(node_id)
+            value = node.get_value()
+            print(f"{name}: {value}")
+        time.sleep(1)
+
+except KeyboardInterrupt:
+    print("Client stopped by user")
+finally:
+    client.disconnect()
