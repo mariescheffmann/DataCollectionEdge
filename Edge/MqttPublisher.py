593d6ec0d2431b62acd6e800a803063683e3880e
mariescheffmann <mariemadsen26@yahoo.dk>
2025-11-20 11:54:45 +0100

    Updted edge to fit with cloud updates to mqtt

--- Edge/MqttPublisher.py
+++ Edge/MqttPublisher.py
@@ -1,12 +1,22 @@
 import paho.mqtt.client as mqtt
 import os
-from dotenv import load_dotenv
+import ssl
 
 class MqttPublisher:
     def __init__(self):
-        load_dotenv()
-        self.client = mqtt.Client(client_id=os.getenv("CLIENT_ID"), protocol=mqtt.MQTTv311)
+        self.client = mqtt.Client(client_id=os.getenv("CLIENT_ID"))
+        self.client.tls_set(
+            ca_certs="/Edge/certs/ca.crt",
+            certfile="/Edge/certs/client.crt",
+            keyfile="/Edge/certs/client.key",
+            tls_version=ssl.PROTOCOL_TLSv1_2
+        )
+        self.client.tls_insecure_set(True)
+
         self.client.connect(os.getenv("BROKER"), int(os.getenv("PORT")), 60)
 
-    def publish(self, message, topic):
+    def publish(self, message, topic_start, topic_suffix):
+        client_id = os.getenv("CLIENT_ID")
+        topic = f"{topic_start}/{client_id}/{topic_suffix}"
         self.client.publish(topic, message)
+        print(message, topic)
